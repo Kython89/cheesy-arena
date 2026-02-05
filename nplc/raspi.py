c4d20423342bf3d13b6d6c6670c7f3e8d5186223
yasen5 <167659798+yasen5@users.noreply.github.com>
2026-02-04 21:39:06 -0800

    Add LED control to BallCounter class, fix timing of ball counter update (NEEDS TESTING)
    Updated BallCounter to include LED control functionality.
    
    Signed-off-by: yasen5 <167659798+yasen5@users.noreply.github.com>

--- nplc/raspi.py
+++ nplc/raspi.py
@@ -4,22 +4,24 @@ from fastapi import FastAPI
 import uvicorn
 
 class BallCounter:
-    def __init__(self, input_pins: list[int], poll_interval: float = 0.1):
+    def __init__(self, led_pin: int, input_pins: list[int], poll_interval: float = 0.1):
         self.input_pins = input_pins
         self.poll_interval = poll_interval
         self.count = 0
         self.active = False
+        self.led_pin = led_pin
 
     def setup_gpio(self) -> None:
         GPIO.setmode(GPIO.BCM)
         for pin in self.input_pins:
             GPIO.setup(pin, GPIO.IN)
+        GPIO.setup(self.led_pin, GPIO.OUT)
 
     async def _read_sensor(self, pin: int) -> None:
         prev_state = True
         while True:
             current_state = GPIO.input(pin)
-            if self.active and current_state and not prev_state:
+            if self.active and not current_state and prev_state:
                 self.count += 1
                 print("Balls Counted:", self.count)
 
@@ -37,10 +39,13 @@ class BallCounter:
     def set_active(self, active: bool) -> None:
         self.active = active
 
+    def set_LED_status(self, active: bool) -> None:
+        GPIO.output(self.led_pin, GPIO.HIGH if active else GPIO.LOW)
+
 # ---------------- FastAPI setup ---------------- #
 
 app = FastAPI()
-counter = BallCounter(input_pins=[17, 22])
+counter = BallCounter(led_pin=-1, input_pins=[17, 22]) # TODO get the led_pin correct
 
 @app.on_event("startup")
 async def startup() -> None:
@@ -64,6 +69,16 @@ def start():
 @app.post("/stop")
 def stop():
     counter.set_active(False)
+    return {"ok": True}
+
+@app.post("/lights_on")
+def lights_on():
+    counter.set_LED_status(True)
+    return {"ok": True}
+
+@app.post("/lights_off")
+def lights_off():
+    counter.set_LED_status(False)
     return {"ok": True}
 
 if __name__ == "__main__":
